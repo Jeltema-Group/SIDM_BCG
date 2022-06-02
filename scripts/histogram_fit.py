8f45fc5f30e8a9535d4a6916f0ac6513fef458cd
Gray-Thoron <104035253+Gray-Thoron@users.noreply.github.com>
2022-06-02 16:34:30 -0700

    Update histogram_fit.py
    Overhauled histogram_fit.py. This entails more customization and an improved fitting function.
    Maximum offset can be set manually, as can the number of bins. The y range changes so the graph fills the majority of the image.
    The reason the bin number is not fixed is because if I set it too low with data with an outlier, all the offsets will end up in one bin.
    The curve is now using scipy.stats.lognorm. Lognorm.fit is used to fit the curve to the unbinned data.

--- scripts/histogram_fit.py
+++ scripts/histogram_fit.py
@@ -12,52 +12,72 @@ from astropy.table import Table
 import sys
 
 
-if len(sys.argv) != 2:
-    print("histogram_fit.py takes one argument: the fits table of results from final_results.py")
+if (len(sys.argv) < 2) or (len(sys.argv) >= 5):
+    print('''histogram_fit.py takes up to three arguments: 
+    1)  The name of the fits table of results from final_results.py.
+    2)  The maximum offset to include. Optional; default is 40.
+    3)  The number (integer) of bins for the histogram. Optional; default is equal to 5.''')
     exit()
 
-results_path = "../results/results_"+sys.argv[1]+".fits"
+if len(sys.argv) == 2:
+    sysargvs=[sys.argv[1], 40, 5]
+elif len(sys.argv) == 3:
+    sysargvs=[sys.argv[1], float(sys.argv[2]), 5]
+elif len(sys.argv) == 4:
+    sysargvs=[sys.argv[1], float(sys.argv[2]), int(sys.argv[3])]
 
-print("Reading results_"+sys.argv[1]+".fits ...")
+print(f'''Inputs:
+    Name of fits table for results: {sysargvs[0]}.fits
+    Maximum offset: {sysargvs[1]} kpc
+    Number of bins: {sysargvs[2]}
+    ''')
+
+results_path = "../results/results_"+sysargvs[0]+".fits"
+
+print("Reading results_"+sysargvs[0]+".fits ...")
 t = Table.read(results_path)
 
-print("Cutting out offsets greater than 40 kpc...")
-mask = (t['BCG_offset_kpc']<40)
+print(f"Cutting out offsets greater than {sysargvs[1]} kpc...")
+mask = (t['BCG_offset_kpc']<sysargvs[1])
 offsets = t['BCG_offset_kpc'][mask]
 p_dist = offsets/np.sum(offsets)
 
-n, bins, p = plt.hist(offsets, bins=3, 
+n, bins, p = plt.hist(offsets, bins=sysargvs[2], 
                         color="tomato", ec="tomato", alpha = 0.5, lw=10,
                         zorder=1)
 
 for item in p:
     item.set_height(item.get_height()/sum(n))
 
-x = [(bins[i]+bins[i+1])/2 for i in range(len(bins)-1)]
 
 from scipy.stats import lognorm
-def f(x, s, u, a):
-    A = [a*(s * xi * np.sqrt(2*np.pi))**(-1) for xi in x]
-    f = [np.exp(-(np.log(xi)**2-np.log(u)**2)/(np.sqrt(2)*s)**2) for xi in x]
-    return [A[i]*f[i] for i in range(len(A))]
-    
-sigma = []
-for counts in n:
-    if counts == 0:
-        sigma += [4.0]
-    else:
-        sigma += [1/counts]
+params = lognorm.fit(offsets, floc=0)
 
-popt, pcov = curve_fit(f, x, n/np.sum(n), sigma=sigma)
+print(f'''Lognorm fit values:
+    mu: {params[2]}
+    sigma: {params[0]}
+    loc: {params[1]}
+    loc should be 0.0.
+    ''')
 
-x = [i/100 for i in range(1, 10000)]
-print("s:", *popt)
+if max(offsets) <= 120:
+    x = np.linspace(1/100, 120, num=12000)
+else:
+    ceilmax = np.ceil(max(offsets))
+    x = np.linspace(1/100, ceilmax, num=100*ceilmax)
 
-print("Plotting the histogram to histogram_fit_" + sys.argv[1] + '.png')
-plt.plot(x, f(x, *popt), color="tomato", linewidth=10, 
-            label="$\mu$="+str(popt[0])[:5]+" $\pm$ "+ str(popt[1])[:4]+ " kpc")
+y = lognorm.pdf(x, np.ones(x.shape)*params[0], params[1], params[2])
 
-plt.ylim(0, 0.6)
+print("Plotting the histogram to histogram_fit_" + sysargvs[0] + '.png')
+plt.plot(x, y, color="tomato", linewidth=10, 
+            label="$\mu$="+str(params[2])[:5]+" $\pm$ "+ str(params[0])[:4]+ " kpc")
+
+if max(n)/sum(n) > max(y):
+    plt.ylim(0, np.ceil(max(n)/sum(n)*10)/10)
+else:
+    plt.ylim(0, np.ceil(max(y)*10)/10)
+
+#plt.ylim(0, 0.6)
 
 fig = plt.gcf()
 fig.set_size_inches(40, 20)
@@ -69,39 +89,4 @@ plt.xticks(fontsize=60)
 plt.yticks(fontsize=60)
 plt.legend()
 
-fig.savefig('../results/results_images/histogram_fit_' + sys.argv[1] + '.png')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fig.savefig('../results/results_images/histogram_fit_' + sysargvs[0] + '.png')
